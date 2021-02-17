2e244523956b82bc294397c6663e7f7b47655d89
Jakub Sokołowski <jakub@status.im>
2021-02-17 11:10:41 +0100

    add secrets.tf and versions.tf
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 0.13.3"
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = " = 1.95.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = " = 2.10.1"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = " = 2.5.1"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = " = 3.42.0"
+    }
+    pass = {
+      source  = "camptocamp/pass"
+      version = " = 1.4.0"
+    }
+  }
+}
