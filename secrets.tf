8053a26a875560af6a264fd2b915bd02f588caca
Jakub Sokołowski <jakub@status.im>
2021-05-02 23:50:28 +0200

    upgrade pass terraform provider to 2.0.0
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- secrets.tf
+++ secrets.tf
@@ -1,5 +1,5 @@
 # Uses PASSWORD_STORE_DIR environment variable
-provider "pass" { refresh_store = false }
+provider "pass" { }
 
 /* Token for interacting with Cloudflare API. */
 data "pass_password" "cloudflare_token" {
