11ad5d4463f6643cd06586fc61a8b0d6923cd067
Jakub Sokołowski <jakub@status.im>
2024-06-24 13:30:11 +0200

    versions.tf: upgrade pass provider to 2.1.1
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     pass = {
       source  = "camptocamp/pass"
-      version = " = 2.0.0"
+      version = " = 2.1.1"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
