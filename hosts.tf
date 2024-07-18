8388f45daac8ef13fcb505ba9ea3e1406f7613cb
Jakub Sokołowski <jakub@status.im>
2024-07-18 12:25:14 +0200

    drop GC and AC hosts, keep only DigitalOcean
    This fleet is only used by legacy application.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts.tf
+++ hosts.tf
@@ -3,19 +3,21 @@
 module "uni_links" {
   source     = "github.com/status-im/infra-tf-multi-provider"
 
-  host_count = var.uni_links_hosts
   name       = "node"
   env        = "uni-links"
   group      = "uni-links"
+
+  do_count = 1
+  ac_count = 0
+  gc_count = 0
 
   open_tcp_ports = ["80", "443"]
 }
 
 resource "cloudflare_record" "join_status_im" {
   zone_id  = lookup(local.zones, "status.im")
-  for_each = toset(module.uni_links.public_ips)
   name     = "join"
-  value    = each.value
+  value    = module.uni_links.public_ips[0]
   type     = "A"
   proxied  = true
 }
--- variables.tf
+++
@@ -1,6 +0,0 @@
-/* SCALING --------------------------------------*/
-
-variable "uni_links_hosts" {
-  description = "Number of hosts to use for universal Status links."
-  default     = 1
-}
